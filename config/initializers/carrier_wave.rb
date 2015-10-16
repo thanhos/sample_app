f169376d256bf3527dec34e156324beae26e2e8a
Thanh Truong <thanhos717@yahoo.com>
2015-10-16 00:55:48 -0700

    Add user microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,7 @@
+CarrierWave.configure do |config|
+  config.azure_storage_account_name = 'micropost'
+  config.azure_storage_access_key = 'Z4Twj5d23sC8+YnFf54+YmujIi0ANfEB4Xd+VMlm398c2GAcxvHS1tSJbD4zBGX8t+I8zB8ose4khvQYxhnEUQ=='
+#  config.azure_storage_blob_host = 'YOUR STORAGE BLOB HOST' # optional
+  config.azure_container = 'azure-container-micropost'
+#  config.asset_host = 'YOUR CDN HOST' # optional
+end

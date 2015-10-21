a7784b279afb2528a944ad5418b0b82299765a28
Thanh Truong <thanhos717@yahoo.com>
2015-10-21 01:42:52 -0700

    Add AWS

--- config/initializers/carrierwave_azure.rb
+++
@@ -1,7 +0,0 @@
-CarrierWave.configure do |config|
-  config.azure_storage_account_name = 'micropost'
-  config.azure_storage_access_key = 'Z4Twj5d23sC8+YnFf54+YmujIi0ANfEB4Xd+VMlm398c2GAcxvHS1tSJbD4zBGX8t+I8zB8ose4khvQYxhnEUQ=='
-#  config.azure_storage_blob_host = 'YOUR STORAGE BLOB HOST' # optional
-  config.azure_container = 'images'
-#  config.asset_host = 'YOUR CDN HOST' # optional
-end
